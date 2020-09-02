28379d9ea5e930e064b30694690ca3da1abc53b2
Charles Arnesus <CharlesArnesus.Matulessy@mitrais.com>
2020-09-02 12:23:11 +0700

    [Charles] set up eureka discovery server

---
+++ src/test/java/com/charles/billing_discovery_server/BillingDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.billing_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
